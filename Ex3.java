af6abe724715a26cca9b823631321096aba459fa
EmanuelB-da-Silva <102166932+EmanuelB-da-Silva@users.noreply.github.com>
2022-03-28 08:57:31 -0300

    Update Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Ex3
 {
 	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
 		
-		int num = entrada.nextInt();
+		int num = EntradaTeclado.leInt();
 	
 	    for(int i = 0; i < num; i++){
 	        for(int j = 0; j < num - i; j++){
